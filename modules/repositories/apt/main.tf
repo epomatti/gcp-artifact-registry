f053e4e845fff8ea15be759a97bcc095cf2a435d
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-04-22 22:57:58 -0300

    feat: public access

--- modules/repositories/apt/main.tf
+++ modules/repositories/apt/main.tf
@@ -4,3 +4,14 @@ resource "google_artifact_registry_repository" "default" {
   format        = "APT"
   mode          = "STANDARD_REPOSITORY"
 }
+
+# https://docs.prismacloud.io/en/enterprise-edition/policy-reference/google-cloud-policies/google-cloud-public-policies/ensure-gcp-artifact-registry-repository-is-not-anonymously-or-publicly-accessible
+resource "google_artifact_registry_repository_iam_binding" "public_binding" {
+  repository = google_artifact_registry_repository.default.name
+  role       = "roles/artifactregistry.reader"
+
+  members = [
+    "allUsers",
+    "allAuthenticatedUsers"
+  ]
+}
