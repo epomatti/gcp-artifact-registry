90334c5fb47a62a4d4f376267f28c02c2893b1c3
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-04-27 13:30:14 -0300

    feat: permissions set to public (anonymous)

--- modules/repositories/apt/main.tf
+++ modules/repositories/apt/main.tf
@@ -6,12 +6,12 @@ resource "google_artifact_registry_repository" "default" {
 }
 
 # https://docs.prismacloud.io/en/enterprise-edition/policy-reference/google-cloud-policies/google-cloud-public-policies/ensure-gcp-artifact-registry-repository-is-not-anonymously-or-publicly-accessible
+# https://cloud.google.com/artifact-registry/docs/access-control#public
 resource "google_artifact_registry_repository_iam_binding" "public_binding" {
   repository = google_artifact_registry_repository.default.name
   role       = "roles/artifactregistry.reader"
 
   members = [
-    "allUsers",
-    "allAuthenticatedUsers"
+    "allUsers"
   ]
 }
